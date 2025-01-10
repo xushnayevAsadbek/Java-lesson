778c5179ec9e4c50da60d64070c58f8421da49a1
asadbek755 <xushnaevasadbek@gmail.com>
2025-01-10 18:47:40 +0500

    intial commit

---
+++ src/App.java
@@ -0,0 +1,14 @@
+import java.lang.Math;
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        String davlat = "Ozbekiston!";
+                System.out.println(davlat);
+        int a=5 ;
+        int b = 6 ;
+        int natija = Math.max(a , b);
+        System.out.println(natija);
+
+    }
+}
+
