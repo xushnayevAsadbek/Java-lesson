a77fe1502cd52ca357391768e7fd5fcf7d2f0035
asadbek755 <xushnaevasadbek@gmail.com>
2025-01-12 01:01:59 +0500

    While loop

--- src/Strings.java
+++ src/Strings.java
@@ -52,10 +52,16 @@ public class Strings {
 //        for (int i=0; i<10; i++){
 //            System.out.println(i + "" + "Salom!");
 //        }
-        for(int i=1; i<10; i++){
-            for (int j=1; j<10; j++){
-                System.out.println(i + "*" + j + "=" +(i * j));
-            }
+//        for(int i=1; i<10; i++){
+//            for (int j=1; j<10; j++){
+//                System.out.println(i + "*" + j + "=" +(i * j));
+//            }
+//        }
+        // While loop
+        int x = 1;
+        while (x<=3){
+            System.out.println(x + "." + "Salom!");
+            x++;
         }
     }
 
