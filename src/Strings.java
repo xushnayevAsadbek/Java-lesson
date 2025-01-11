7359aa9f8f2ceb0aa10dd273dda2a74ed55e385c
asadbek755 <xushnaevasadbek@gmail.com>
2025-01-11 23:54:18 +0500

    String qoshish

--- src/Strings.java
+++ src/Strings.java
@@ -7,6 +7,11 @@ public class Strings {
         String text = "Hello World Lorem Ipsum is simply dummy text of the printing and typesetting industry";
         System.out.println(text.indexOf("World"));
         System.out.println(text.length());
+        // Stringlarni qoshish
+        String text2 = "Lorem ipsum  dummy text of ";
+        String text3 = "Lorem ipsum fffx dfdcdf";
+        System.out.println(text2 + text3);
+        System.out.println(text2.concat(text3));
     }
 
 }
