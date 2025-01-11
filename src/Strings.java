f5a19e0342ff231b2a8a9be272afe4de36f565b3
asadbek755 <xushnaevasadbek@gmail.com>
2025-01-12 00:49:05 +0500

    For loop

--- src/Strings.java
+++ src/Strings.java
@@ -48,6 +48,15 @@ public class Strings {
                System.out.println("Bunday so'z yoq");
        }
         }
+        //For loop
+//        for (int i=0; i<10; i++){
+//            System.out.println(i + "" + "Salom!");
+//        }
+        for(int i=1; i<10; i++){
+            for (int j=1; j<10; j++){
+                System.out.println(i + "*" + j + "=" +(i * j));
+            }
+        }
     }
 
 }
