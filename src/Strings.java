f0a62d7307c027e5d1e5df52ed81a604b88443bd
asadbek755 <xushnaevasadbek@gmail.com>
2025-01-12 00:04:59 +0500

    Shart operatorlari

--- src/Strings.java
+++ src/Strings.java
@@ -1,17 +1,26 @@
 public class Strings {
     public static void main (String[] args){
-        String ism = "Akmal";
-        System.out.println(ism.length());
-        System.out.println(ism.toUpperCase());
-        System.out.println(ism.toLowerCase());
-        String text = "Hello World Lorem Ipsum is simply dummy text of the printing and typesetting industry";
-        System.out.println(text.indexOf("World"));
-        System.out.println(text.length());
+//        String ism = "Akmal";
+//        System.out.println(ism.length());
+//        System.out.println(ism.toUpperCase());
+//        System.out.println(ism.toLowerCase());
+//        String text = "Hello World Lorem Ipsum is simply dummy text of the printing and typesetting industry";
+//        System.out.println(text.indexOf("World"));
+//        System.out.println(text.length());
         // Stringlarni qoshish
-        String text2 = "Lorem ipsum  dummy text of ";
-        String text3 = "Lorem ipsum fffx dfdcdf";
-        System.out.println(text2 + text3);
-        System.out.println(text2.concat(text3));
+//        String text2 = "Lorem ipsum  dummy text of ";
+//        String text3 = "Lorem ipsum fffx dfdcdf";
+//        System.out.println(text2 + text3);
+//        System.out.println(text2.concat(text3));
+        int number1 = 6 ;
+        int number2 = 5 ;
+        if (number1 >number2){
+            System.out.println(number1);
+        } else if (number2 == number1) {
+            System.out.println(number2);
+        }else{
+            System.out.println(0);
+        }
     }
 
 }
