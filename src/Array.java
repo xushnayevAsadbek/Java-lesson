e1353e6ab9abf4afa110cb3e5bc6284319e7a31c
asadbek755 <xushnaevasadbek@gmail.com>
2025-01-12 12:54:51 +0500

    Array commit

---
+++ src/Array.java
@@ -0,0 +1,9 @@
+public class Array {
+    public  static void main(String[] args) {
+        String[] cars = {"mers" , "bwm" , "volvo", "gm"};
+        System.out.println(cars[2]);
+        cars[2]="mazda";
+        System.out.println(cars[2]);
+        System.out.println(cars.length);
+    }
+}
