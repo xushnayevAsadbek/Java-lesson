df557e0941776634c5474d01cb9fd56a737aeb07
asadbek755 <xushnaevasadbek@gmail.com>
2025-01-12 13:16:26 +0500

    Array for loop and for each commit

--- src/Array.java
+++ src/Array.java
@@ -1,9 +1,17 @@
 public class Array {
     public  static void main(String[] args) {
         String[] cars = {"mers" , "bwm" , "volvo", "gm"};
-        System.out.println(cars[2]);
-        cars[2]="mazda";
-        System.out.println(cars[2]);
-        System.out.println(cars.length);
+//        System.out.println(cars[2]);
+//        cars[2]="mazda";
+//        System.out.println(cars[2]);
+//        System.out.println(cars.length);
+        // For loop
+//        for (int i = 0; i < cars.length; i++) {
+//            System.out.println(cars[i]);
+//        }
+        // for each
+        for (String car : cars) {
+            System.out.println(car);
+        }
     }
 }
