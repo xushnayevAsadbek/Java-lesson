a37706e8e2ff6993fb2c50c0db309e28b8d1fe21
asadbek755 <xushnaevasadbek@gmail.com>
2025-01-21 22:47:08 +0500

    Java OOP

---
+++ src/ClassOOp.java
@@ -0,0 +1,12 @@
+public class ClassOOp {
+    static String personPlus(String ism) {
+        return ism + " uchun!";
+    }
+    public static void main(String[] args) {
+    String add = personPlus("aaa");
+    String bbb = personPlus("fff");
+        System.out.println(add);
+        System.out.println(bbb);
+    }
+
+}
