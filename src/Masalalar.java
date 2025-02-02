ba4b7d54f411966fddc7ae918a72de8845a88370
Asadbek Xushnayev <xushnaevasadbek@gmail.com>
2025-02-02 21:23:04 +0500

    masala davomi

--- src/Masalalar.java
+++ src/Masalalar.java
@@ -1,13 +1,22 @@
 import java.util.Scanner;
 public class Masalalar {
     public static void main(String[] args) {
+        int a , b , S , P ;
         Scanner scanner = new Scanner(System.in);
-        System.out.println(  "tomonlarini kiriting:");
+        System.out.print("A=");
+        a = scanner.nextInt();
+        System.out.print("B=");
+        b = scanner.nextInt();
+        S= a * b;
+        P= 2*(a+b);
+        System.out.println("Yuzi" + S);
+        System.out.println("Perimetri" + P);
+        // System.out.println(  "tomonlarini kiriting:");
     //    int a=5;
-       int Perimetri ;
-        Perimetri = scanner.nextInt();
-        Perimetri = Perimetri * 4;
-       System.out.println("Perimetr: " + Perimetri);
+    //    int Perimetri ;
+    //     Perimetri = scanner.nextInt();
+    //     Perimetri = Perimetri * 4;
+    //    System.out.println("Perimetr: " + Perimetri);
 
     }
 }
