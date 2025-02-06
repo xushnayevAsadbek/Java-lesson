bf064718ae1a96522a5523d75b0c0bee3b2aacb8
Asadbek Xushnayev <xushnaevasadbek@gmail.com>
2025-02-06 20:39:37 +0500

    ok

--- src/abstraction/App.java
+++ src/abstraction/App.java
@@ -2,17 +2,28 @@ package abstraction;
  abstract class InnerApp {
  public abstract void Chizmoq ();
  }
+ abstract class Hayvon  {
+    public abstract void Ovoz ();    
+ }
+class It extends Hayvon{
+    public void Ovoz(){
+        System.out.println("Wow -wow ");
+    }
+}
  class Aylana extends InnerApp{
     int r ;
     Aylana(int r){
         this.r = r;
     }
     public void Chizmoq(){
-        System.out.println(this.r+" ga teng aylanani chizmoq");
+        System.out.println(this.r+" sm teng aylanani chizmoq");
  }
 }
 public class App {
     public static void main(String[] args) {
-        System.out.println();
+        Aylana aylana = new Aylana(10);
+        aylana.Chizmoq();
+        It it = new It();
+        it.Ovoz();
     }
 }
