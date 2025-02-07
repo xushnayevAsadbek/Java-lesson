325113463df36389429b164bc46f4c10f7125050
Asadbek Xushnayev <xushnaevasadbek@gmail.com>
2025-02-07 21:08:38 +0500

    linked list commit

---
+++ src/linkedList/App.java
@@ -0,0 +1,10 @@
+package linkedList;
+import java.util.LinkedList;
+public class App {
+    public static void main(String[] args) {
+       LinkedList <String> cars = new LinkedList<String>() ;
+       cars.add("BMW");
+       cars.add("Matiz");
+       System.out.println(cars);
+    }
+}
