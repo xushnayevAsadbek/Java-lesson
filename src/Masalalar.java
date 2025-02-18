e65eae577d025f62681a975606ae429300c8c75b
Asadbek Xushnayev <xushnaevasadbek@gmail.com>
2025-02-18 17:04:24 +0500

    if else ga oid masalalar

--- src/Masalalar.java
+++ src/Masalalar.java
@@ -1,22 +1,31 @@
 import java.util.Scanner;
 public class Masalalar {
     public static void main(String[] args) {
-        int a , b , S , P ;
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("A=");
-        a = scanner.nextInt();
-        System.out.print("B=");
-        b = scanner.nextInt();
-        S= a * b;
-        P= 2*(a+b);
-        System.out.println("Yuzi" + S);
-        System.out.println("Perimetri" + P);
+//        int a , b , S , P ;
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("A=");
+//        a = scanner.nextInt();
+//        System.out.print("B=");
+//        b = scanner.nextInt();
+//        S= a * b;
+//        P= 2*(a+b);
+//        System.out.println("Yuzi" + S);
+//        System.out.println("Perimetri" + P);
         // System.out.println(  "tomonlarini kiriting:");
     //    int a=5;
     //    int Perimetri ;
     //     Perimetri = scanner.nextInt();
     //     Perimetri = Perimetri * 4;
     //    System.out.println("Perimetr: " + Perimetri);
+        int a= 12;
+        if (a>10){
+            System.out.println(a+3);
+        }  else if (a<10) {
+            System.out.println(a*2);
+        }      else{
+            System.out.println(22);
+        }
 
+        System.out.println("End of the program");
     }
 }
---
+++ src/Misollar/ifmisollar/App.java
@@ -0,0 +1,51 @@
+package Misollar.ifmisollar;
+
+public class App {
+    public static void main(String[] args) {
+        // 1-misol
+        // int a= 9;
+        // if (a>10){
+        //     System.out.println(a+3);
+        // }  else if (a<10) {
+        //     System.out.println(a*2);
+        // }      else{
+        //     System.out.println(22);
+        // }
+        
+        // 2-misol
+        // int b = 7;
+        // int c = b/3;
+        // System.out.println(c);
+
+        // 3-misol
+        // int b = 7;
+        // int c = b%3;
+        // System.out.println(c);
+
+        // 4-misol
+
+        // int a = 6;
+        // if (a%2 == 0){
+        //     System.out.println("juft son");
+        
+        // } else{
+        //     System.out.println("toq son");
+        // }
+
+        // 5-Misollar
+
+        // int a=9 , b=10;
+        // if (a>b) {
+        //     System.out.println("a katta b son dan ");
+        // } else if (a<b) {
+        //     System.out.println("b katta a son dan ");
+        // } else {
+        //     System.out.println("a va b sonlar teng");
+            
+        // } 
+
+        // 6-misol
+
+
+    }
+}
