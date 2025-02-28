46967ff370af966cc4563d8efab29952ea5990ff
Asadbek Xushnayev <xushnaevasadbek@gmail.com>
2025-02-28 10:27:52 +0500

    for oid misollar

---
+++ src/Misollar/formisolar/App.java
@@ -0,0 +1,59 @@
+package Misollar.formisolar;
+
+public class App {
+    public static void main(String[] args) {
+        //  1-misol
+        // int a = 2;
+        // int sum = 0 ;
+        // for(int i=0 ;  i<=a; i++ ){
+        //     sum = sum + i;
+        // }
+        // System.out.println(sum);
+
+        // 2-misol
+
+    // int a = 2;
+    //     int sum = 0 ;
+        
+    //     for(int i=0 ;  i<=a; i++ ){
+           
+    //         if (i%2 ==0) {
+    //          sum = sum + i;
+    //         }
+    //     }
+    //     System.out.println(sum);
+    // }
+
+    // 3-misol 
+
+    // int a = 2 ;
+    // int sum = 0;
+    // for(int i = 0 ; i<=a ; i++){
+    //     if (i%2 !=0 ) {
+    //         sum = sum + i;
+    //     }
+    // }
+    // System.out.println(sum );
+
+        //  4-misol 
+
+        // int a = 35;
+        // int sum = 0;
+        // for (int i=0 ; i<=a; i++){
+        //     if (i%5 == 0) {
+        //         sum = sum + i;
+        //     }
+        // }
+        // System.out.println(sum);
+
+        int a =10 ;
+        int count =0;
+        for(int  i= 1 ; i<=a ; i++){
+            if (i%2 ==0) {
+                count ++;
+            }
+        }
+        System.out.println(count);
+
+}
+}
