a1cc067cc4cdd9e6a9ee501673bb3543d5595419
Asadbek Xushnayev <xushnaevasadbek@gmail.com>
2025-02-28 10:54:09 +0500

    for oid misollar (davomi)

--- src/Misollar/formisolar/App.java
+++ src/Misollar/formisolar/App.java
@@ -46,14 +46,85 @@ public class App {
         // }
         // System.out.println(sum);
 
-        int a =10 ;
-        int count =0;
-        for(int  i= 1 ; i<=a ; i++){
-            if (i%2 ==0) {
-                count ++;
-            }
-        }
-        System.out.println(count);
+        // 5-misol
+        // int a =10 ;
+        // int count =0;
+        // for(int  i= 1 ; i<=a ; i++){
+        //     if (i%2 ==0) {
+        //         count ++;
+        //     }
+        // }
+        // System.out.println(count);
 
+        // 6-misol
+        // int a =2;
+        // int b= 8;
+        // int sum =0;
+        // for(int i=a; i<b; i++){
+        //     sum = sum +i ;
+        // }
+        // System.out.println(sum );
+
+        // 7-misol 
+
+        // int a = 4 , b=15;
+        // int  count =0;
+        
+        // for(int i=a ; i<b; i++){
+        //     if (i%2 ==0) {
+        //        count ++;
+
+        //     }
+        // }
+        // System.out.println(count);
+
+        // 8-misol
+
+        // 7-misol 
+
+        // int a = 4 , b=15;
+        // int sum =0 , count =0;
+        
+        // for(int i=a ; i<b; i++){
+        //     if (i%3 ==0) {
+        //        count ++;
+        //        sum = sum + i;
+
+        //     }
+        // }
+        // System.out.println(sum);
+        // System.out.println(count);
+
+        // 8-misol 
+        // int a = 6 , b=16;
+        // int  count =0;
+        
+        // for(int i=a ; i<b; i++){
+        //     if (i%2 ==0 && i% 3 ==0) {
+        //        count ++;
+
+        //     }
+        // }
+        // System.out.println(count);
+
+        // 9- misol 
+        // int a = -1 , b=10;
+        // int  count =0;
+        
+        // for(int i=a ; i<=b; i++){
+        //     if (i>0) {
+        //        count ++;
+
+        //     }
+        // }
+        // System.out.println(count);  
+
+        // 10-misol 
+        int a = 2 , p=1;
+        
+        for(int i=1; i<=5; i++){
+         p= p*a;   
+        }
+        System.out.println(p);  
 }
 }
